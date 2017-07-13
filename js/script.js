2c117e991e8f0a939b7ee6e8a8b49adae8ba90a7
ottenokLeshi <ottenok.belogo@gmail.com>
2017-07-14 00:52:10 +0300

    added scrolling lib

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$( document ).ready(function() {
+	let scrolledPrev = 0;
+	let scrollContainers = [];
+	$(".scroll").each(function() {
+	    scrollContainers.push($(this)[0]);
+	});
+	console.log(scrollContainers)  
+
+    window.onscroll = function() {
+		let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+		const cutPosition = findCut(scrollContainers.map(container => $(container).offset().top - 50), scrolled);
+		if (scrolledPrev - scrolled < 0){
+			console.log("Down "+ scrolled)
+		}	else {
+			console.log("Up "+ scrolled)
+		}
+		scrolledPrev = scrolled;
+	}
+});
+
+const findCut = (array, elem) => {
+	let begin = 0;
+	let end = array.length - 1;
+	let middle = 0;
+
+	while (end - begin > 1) {
+		middle = Math.floor((end + begin)/2);
+		if (elem < array[middle]) {
+			end = middle;
+		} else {
+			begin = middle
+		}
+	}
+	return [begin, end]
+}*/
